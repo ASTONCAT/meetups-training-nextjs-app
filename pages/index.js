99d4281579e9930fa384f508bbb1edc836371b5d
Koder Astoncat <koder@astoncat.com>
2022-07-09 17:26:41 +0200

    Rendering A List Of (Dummy) Meetups

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import MeetupList from "../components/meetups/MeetupList"
+
+const DUMMY_MEETUPS = [
+    {
+        id: "m1",
+        title: "A First Meetup",
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        address: "Marienplatz 1, 80331 Munich, Germany",
+        description: "This is a first meetup!"
+    },
+    {
+        id: "m1",
+        title: "A Second Meetup",
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        address: "Marienplatz 4, 80331 Munich, Germany",
+        description: "This is a first meetup!"
+    },
+    {
+        id: "m1",
+        title: "A Third Meetup",
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        address: "Marienplatz 8, 80331 Munich, Germany",
+        description: "This is a first meetup!"
+    }
+]
+
+function HomePage() {
+    return <MeetupList meetups={DUMMY_MEETUPS} />
+}
+
+export default HomePage
