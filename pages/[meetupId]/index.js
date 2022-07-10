2dc0bd23a1a6abc68289b16bba8723e455681091
Koder Astoncat <koder@astoncat.com>
2022-07-10 19:36:19 +0200

    Adding Custom Components & Styling With CSS

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail'
+
+function MeetupDetails() {
+	return (
+		<MeetupDetail
+			img="https://www.klatovy.net/fotogalerie/albums/userpics2010/IMG_2547.jpg"
+			title="Klatovy Folklore Festival"
+			address="Náměstí Míru 1, Klatovy 33901"
+			description="After the end of the traditional pilgrimage, the folklore festival begins in Klatovy. We will meet in the restaurant of the Družba cultural house."
+		/>
+	)
+}
+
+export default MeetupDetails
