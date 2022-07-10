b8f2a5f51c02890d5ab278eadf59581b5b63cfee
Koder Astoncat <koder@astoncat.com>
2022-07-10 12:32:57 +0200

    Adding A Form For Adding Meetups

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,34 @@
-import MeetupList from "../components/meetups/MeetupList"
+import MeetupList from '../components/meetups/MeetupList'
 
 const DUMMY_MEETUPS = [
-    {
-        id: "m1",
-        title: "A First Meetup",
-        image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-        address: "Marienplatz 1, 80331 Munich, Germany",
-        description: "This is a first meetup!"
-    },
-    {
-        id: "m1",
-        title: "A Second Meetup",
-        image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-        address: "Marienplatz 4, 80331 Munich, Germany",
-        description: "This is a first meetup!"
-    },
-    {
-        id: "m1",
-        title: "A Third Meetup",
-        image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-        address: "Marienplatz 8, 80331 Munich, Germany",
-        description: "This is a first meetup!"
-    }
+	{
+		id: 'm1',
+		title: 'A First Meetup',
+		image:
+			'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+		address: 'Marienplatz 1, 80331 Munich, Germany',
+		description: 'This is a first meetup!'
+	},
+	{
+		id: 'm2',
+		title: 'A Second Meetup',
+		image:
+			'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+		address: 'Marienplatz 4, 80331 Munich, Germany',
+		description: 'This is a first meetup!'
+	},
+	{
+		id: 'm3',
+		title: 'A Third Meetup',
+		image:
+			'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+		address: 'Marienplatz 8, 80331 Munich, Germany',
+		description: 'This is a first meetup!'
+	}
 ]
 
 function HomePage() {
-    return <MeetupList meetups={DUMMY_MEETUPS} />
+	return <MeetupList meetups={DUMMY_MEETUPS} />
 }
 
 export default HomePage
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,11 @@
+import NewMeetupForm from '../../components/meetups/NewMeetupForm'
 // our-domain.com/new-meetup
+
+function NewMeetupPage() {
+	function addMeetupHandler(enteredMeetupData) {
+		console.log(enteredMeetupData)
+	}
+	return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+}
+
+export default NewMeetupPage
