b7fe0cf45e27c9ff9f5ef07500faf1e4def6057d
Koder Astoncat <koder@astoncat.com>
2022-07-12 11:43:18 +0200

    Dynamic Path Params In getStaticProps

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,26 @@ function MeetupDetails() {
 	)
 }
 
+export async function getStaticProps(context) {
+	// fetch data for a single meetup
+
+	const meetupId = context.params.meetupId
+
+	console.log(meetupId)
+
+	return {
+		props: {
+			meetupData: {
+				id: 'm1',
+				image:
+					'https://www.klatovy.net/fotogalerie/albums/userpics2010/IMG_2547.jpg',
+				title: 'Klatovy Folklore Festival',
+				address: 'Náměstí Míru 1, Klatovy 33901',
+				description:
+					'After the end of the traditional pilgrimage, the folklore festival begins in Klatovy. We will meet in the restaurant of the Družba cultural house.'
+			}
+		}
+	}
+}
+
 export default MeetupDetails
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import MeetupList from '../components/meetups/MeetupList'
 
 const DUMMY_MEETUPS = [
@@ -28,13 +27,32 @@ const DUMMY_MEETUPS = [
 	}
 ]
 
-function HomePage() {
-	const [loadedMeetups, setLoadedMeetups] = useState([])
-	useEffect(() => {
-		// send a http request and fetch data
-		setLoadedMeetups(DUMMY_MEETUPS)
-	}, [])
-	return <MeetupList meetups={loadedMeetups} />
+function HomePage(props) {
+	return <MeetupList meetups={props.meetups} />
 }
 
+export async function getStaticProps() {
+	// fetch data from an API or databaze or files
+	return {
+		props: {
+			meetups: DUMMY_MEETUPS
+		},
+		revalidate: 3600
+	}
+}
+
+// export async function getServerSideProps(context) {
+// 	const req = context.req
+// 	const res = context.res
+
+// 	// fetch data from an API or databaze or files
+
+// 	return {
+// 		props: {
+// 			meetups: DUMMY_MEETUPS
+// 		}
+// 	}
+// }
+
 export default HomePage
+ 
